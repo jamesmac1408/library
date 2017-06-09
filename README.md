c64e1c1088a8b4d0e4ae30b2cf701b45f73f5579
Alex Saunders <alex.saunders@shopdirect.com>
2017-06-09 15:01:51 +0100

    style updates

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - Each component contained in a seperate folder within 'components/'
 - 2 sub-folders: JS & SASS containing the component.js and component.scss for that component
 - README.md: each component contains a README.md file which details how to use the component 
-- Demo folder containing index.html, and demo.js if required.
+- Demo folder containing index.html (with front-matter), and js/demo.js & scss/demo.scss if required.
 
 ## Folder structure for each component
 ```
--- demo/build.js
+++ demo/build.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs')
 const path = require('path')
+const fm = require('front-matter');
 
 const componentDirs = fs.readdirSync('components').filter((f) => fs.statSync(path.join('components', f)).isDirectory());
 
@@ -11,13 +12,14 @@ const footer = fs.readFileSync(path.join('demo', 'partials', 'footer.html'), 'ut
 let html = header;
 
 for (let dir of componentDirs) {
-  const demoHTML = fs.readFileSync(path.join('components', dir, 'demo', 'index.html'), 'utf8');
+  const demoObj = fm(fs.readFileSync(path.join('components', dir, 'demo', 'index.html'), 'utf8'));
   // TODO: Use YAML or something similar to get component name/attributes
   html += `
-    <h2 class="component-title">${dir}</h2>
+    <h2 class="component-title">${demoObj.attributes.name}</h2>
     <div class="demo-container">
   `
-  html += demoHTML;
+  html += demoObj.body;
+  console.log(demoObj)
   html += `</div>`;
 }
 
