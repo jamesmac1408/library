f0c43b981cc0ac56dcc600bbb72a7835b5e08b68
Alex Saunders <alex.saunders@shopdirect.com>
2017-07-11 12:21:08 +0100

    demo updates

--- docs/components/carousels/demo-assets/demo.js
+++ docs/components/carousels/demo-assets/demo.js
@@ -2,7 +2,7 @@
 if (document.getElementById('sliderTarget')) {
   var carousel = new loopedCarousel('sliderTarget', {
     type: 'creative',
-    autoplay: false,
-    autoplaySpeed: 100,
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 }
